cc7280f85ac14ff414aa3279dcc6dee8af9b61c7
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-15 10:35:26 -0500

    Added delete functionality to the owners, employees, and animals.

--- src/components/animals/AnimalList.js
+++ src/components/animals/AnimalList.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
+import dog from './DogIcon.svg'
+import "./animal.css"
 
 class AnimalList extends Component {
     render() {
         return (
-            <section className="content">
-            <h2>Animal List</h2>
+            <section className="content animals">
             {
-                this.props.animals.map(animal => 
-                    <div key={animal.id}>
-                       <p>{animal.name}</p>
+                this.props.animals.map(animal =>
+                    <div key={animal.id} className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                <img src={dog} alt="Dog" className="icon--dog" />
+                                {animal.name}
+                                <button
+                                    onClick={() => this.props.deleteAnimal(animal.id)}
+                                    className="card-link">Delete</button>
+                            </h5>
+                        </div>
                     </div>
                 )
             }
--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
+import "./employee.css"
 
 
 class EmployeeList extends Component {
     render() {
         return (
-            <section className="content">
-            <h2>Employee List</h2>
+            <section className="employees content">
             {
                 this.props.employees.map(employee =>
-                    <div key={employee.id}>
-                        {employee.name}
+                    <div key={employee.id} className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                {employee.name}
+                                <button
+                                    onClick={() => this.props.deleteEmployee(employee.id)}
+                                    className="card-link">Delete</button>
+                            </h5>
+                        </div>
                     </div>
                 )
             }
